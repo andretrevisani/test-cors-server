0ee97c384f2648adcfbd73e488613016740c10a0
André Trevisani <ad.trevisani@gmail.com>
2024-01-16 15:01:51 -0400

    Adding CORP

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-var cors = require('cors')
+// var cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-var corsOptions = {}
+// var corsOptions = {}
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use(express.static('fonts'))
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
